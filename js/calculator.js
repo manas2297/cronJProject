04085b83a1e64e5b7d5e1f6539b79edbccca9853
manas2297 <yadavmanas22@gmail.com>
2019-06-21 15:44:21 +0530

    Added reset after =

--- js/calculator.js
+++ js/calculator.js
@@ -1,13 +1,24 @@
 let displayValue;
+let flag=0;
 let result;
 function inputNumber(num){
-	displayValue = document.calculator.userInput.value;
+    if(flag===1){
+        reset();
+        displayValue = document.calculator.userInput.value;
 	// console.log(displayValue);
-	document.calculator.userInput.value = displayValue + num;
+    document.calculator.userInput.value = displayValue + num;
+    flag=0;
+    }else{
+        displayValue = document.calculator.userInput.value;
+	// console.log(displayValue);
+    document.calculator.userInput.value = displayValue + num;
+    }
+	
 }
 function calculate(){
 	displayValue = document.calculator.userInput.value;
-	result = eval(displayValue);
+    result = eval(displayValue);
+    flag =1;
 	
 	if(displayValue.includes("/0")){
 		document.calculator.userInput.value="ERROR";
@@ -21,5 +32,8 @@ function calculate(){
 //reset
 function reset(){
 	let clearInput = "";
-	document.calculator.userInput.value = clearInput;
+    document.calculator.userInput.value = clearInput;
+    // console.log(flag);
+    
+    
 }
